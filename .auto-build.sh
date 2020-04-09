eae465ac42993b1de49de99f6891b97d49e6122e
Erriez <erriez@users.noreply.github.com>
2020-04-09 19:46:04 +0200

    Fix .auto-build.sh script

--- .auto-build.sh
+++ .auto-build.sh
@@ -1,20 +1,52 @@
-#!/bin/sh
+#/bin/bash
 
-################################################################################
-# Title         : .auto-build.sh
-# Date created  : 2 August 2018
-__AUTHOR__="Erriez"
-#
-# This script will start PlatformIO build.
-#
-################################################################################
-
-################################################################################
-##### Setup this script and get the current gh-pages branch.               #####
-echo 'Setting up the script...'
-
-# Exit with nonzero exit code if anything fails
+# Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Build sources
-platformio ci --lib="." --project-conf=platformio/platformio.ini examples/LCDKeypadShield/LCDKeypadShield.ino
+echo "Starting auto-build script..."
+
+
+function autobuild()
+{
+    # Set environment variables
+    BOARDS_AVR="--board uno --board megaatmega2560 --board leonardo"
+
+    echo "Install libraries"
+    platformio lib -g install "LiquidCrystal"
+
+    echo "Build examples..."
+    platformio ci \
+        --lib="." \
+        ${BOARDS_AVR} \
+        --project-option="lib_deps=Wire" \
+        examples/LCDKeypadShield/LCDKeypadShield.ino
+}
+
+function generate_doxygen()
+{
+    echo "Generate Doxygen HTML..."
+
+    DOXYGEN_PDF="ErriezLCDKeypadShield.pdf"
+
+    # Cleanup
+    rm -rf html latex
+
+    # Generate Doxygen HTML and Latex
+    doxygen Doxyfile
+
+    # Allow filenames starting with an underscore    
+    echo "" > html/.nojekyll
+
+    # Generate PDF when script is not running on Travis-CI
+    if [[ -z ${TRAVIS_BUILD_DIR} ]]; then
+        # Generate Doxygen PDF
+        make -C latex
+
+        # Copy PDF to root directory
+        cp latex/refman.pdf ./${DOXYGEN_PDF}
+    fi
+}
+
+autobuild
+generate_doxygen
+
